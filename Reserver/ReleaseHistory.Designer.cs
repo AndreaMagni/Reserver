ff6c2423e91f353f344edad6a804b45e015425c9
Katjie <zappa.arianna@gmail.com>
2020-07-10 00:01:20 +0200

    #8 Tab storico rilasci
    Creazione grid storico rilasci + filtri

--- Reserver/ReleaseHistory.Designer.cs
+++ Reserver/ReleaseHistory.Designer.cs
@@ -28,54 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle19 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle20 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle21 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.metroGridReleaseHistory = new MetroFramework.Controls.MetroGrid();
+            this.groupBoxFilter = new System.Windows.Forms.GroupBox();
+            this.metroComboBoxUsers = new MetroFramework.Controls.MetroComboBox();
+            this.metroButtonFilter = new MetroFramework.Controls.MetroButton();
+            this.metroComboBoxServers = new MetroFramework.Controls.MetroComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.metroGridReleaseHistory)).BeginInit();
+            this.groupBoxFilter.SuspendLayout();
             this.SuspendLayout();
             // 
-            // metroTextBox1
+            // metroGridReleaseHistory
             // 
+            this.metroGridReleaseHistory.AllowUserToAddRows = false;
+            this.metroGridReleaseHistory.AllowUserToDeleteRows = false;
+            this.metroGridReleaseHistory.AllowUserToResizeRows = false;
+            this.metroGridReleaseHistory.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.metroGridReleaseHistory.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.metroGridReleaseHistory.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.metroGridReleaseHistory.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle19.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle19.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(180)))), ((int)(((byte)(197)))));
+            dataGridViewCellStyle19.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle19.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle19.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(180)))), ((int)(((byte)(197)))));
+            dataGridViewCellStyle19.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle19.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.metroGridReleaseHistory.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle19;
+            this.metroGridReleaseHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle20.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle20.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle20.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle20.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle20.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(134)))), ((int)(((byte)(163)))));
+            dataGridViewCellStyle20.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle20.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.metroGridReleaseHistory.DefaultCellStyle = dataGridViewCellStyle20;
+            this.metroGridReleaseHistory.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.metroGridReleaseHistory.EnableHeadersVisualStyles = false;
+            this.metroGridReleaseHistory.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.metroGridReleaseHistory.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.metroGridReleaseHistory.Location = new System.Drawing.Point(20, 109);
+            this.metroGridReleaseHistory.Name = "metroGridReleaseHistory";
+            this.metroGridReleaseHistory.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle21.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle21.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(180)))), ((int)(((byte)(197)))));
+            dataGridViewCellStyle21.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle21.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle21.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(180)))), ((int)(((byte)(197)))));
+            dataGridViewCellStyle21.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle21.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.metroGridReleaseHistory.RowHeadersDefaultCellStyle = dataGridViewCellStyle21;
+            this.metroGridReleaseHistory.RowHeadersWidth = 15;
+            this.metroGridReleaseHistory.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.metroGridReleaseHistory.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.metroGridReleaseHistory.Size = new System.Drawing.Size(640, 274);
+            this.metroGridReleaseHistory.TabIndex = 43;
             // 
+            // groupBoxFilter
             // 
+            this.groupBoxFilter.Controls.Add(this.metroComboBoxUsers);
+            this.groupBoxFilter.Controls.Add(this.metroButtonFilter);
+            this.groupBoxFilter.Controls.Add(this.metroComboBoxServers);
+            this.groupBoxFilter.Location = new System.Drawing.Point(20, 22);
+            this.groupBoxFilter.Name = "groupBoxFilter";
+            this.groupBoxFilter.Size = new System.Drawing.Size(639, 66);
+            this.groupBoxFilter.TabIndex = 44;
+            this.groupBoxFilter.TabStop = false;
+            this.groupBoxFilter.Text = "Filtri";
             // 
-            this.metroTextBox1.CustomButton.Image = null;
-            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(53, 1);
-            this.metroTextBox1.CustomButton.Name = "";
-            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
-            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
-            this.metroTextBox1.CustomButton.TabIndex = 1;
-            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
-            this.metroTextBox1.CustomButton.UseSelectable = true;
-            this.metroTextBox1.CustomButton.Visible = false;
-            this.metroTextBox1.Lines = new string[] {
-        "metroTextBox1"};
-            this.metroTextBox1.Location = new System.Drawing.Point(565, 362);
-            this.metroTextBox1.MaxLength = 32767;
-            this.metroTextBox1.Name = "metroTextBox1";
-            this.metroTextBox1.PasswordChar = '\0';
-            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
-            this.metroTextBox1.SelectedText = "";
-            this.metroTextBox1.SelectionLength = 0;
-            this.metroTextBox1.SelectionStart = 0;
-            this.metroTextBox1.ShortcutsEnabled = true;
-            this.metroTextBox1.Size = new System.Drawing.Size(75, 23);
-            this.metroTextBox1.TabIndex = 8;
-            this.metroTextBox1.Text = "metroTextBox1";
-            this.metroTextBox1.UseSelectable = true;
-            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // metroComboBoxUsers
+            // 
+            this.metroComboBoxUsers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.metroComboBoxUsers.FormattingEnabled = true;
+            this.metroComboBoxUsers.ItemHeight = 23;
+            this.metroComboBoxUsers.Location = new System.Drawing.Point(248, 21);
+            this.metroComboBoxUsers.Margin = new System.Windows.Forms.Padding(5);
+            this.metroComboBoxUsers.Name = "metroComboBoxUsers";
+            this.metroComboBoxUsers.Size = new System.Drawing.Size(182, 29);
+            this.metroComboBoxUsers.TabIndex = 46;
+            this.metroComboBoxUsers.UseSelectable = true;
+            // 
+            // metroButtonFilter
+            // 
+            this.metroButtonFilter.Cursor = System.Windows.Forms.Cursors.Default;
+            this.metroButtonFilter.Location = new System.Drawing.Point(546, 23);
+            this.metroButtonFilter.Margin = new System.Windows.Forms.Padding(5);
+            this.metroButtonFilter.Name = "metroButtonFilter";
+            this.metroButtonFilter.Size = new System.Drawing.Size(75, 23);
+            this.metroButtonFilter.TabIndex = 45;
+            this.metroButtonFilter.Text = "Filtra";
+            this.metroButtonFilter.UseSelectable = true;
+            this.metroButtonFilter.Click += new System.EventHandler(this.metroButtonFilter_Click);
+            // 
+            // metroComboBoxServers
+            // 
+            this.metroComboBoxServers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.metroComboBoxServers.FormattingEnabled = true;
+            this.metroComboBoxServers.ItemHeight = 23;
+            this.metroComboBoxServers.Location = new System.Drawing.Point(8, 21);
+            this.metroComboBoxServers.Margin = new System.Windows.Forms.Padding(5);
+            this.metroComboBoxServers.Name = "metroComboBoxServers";
+            this.metroComboBoxServers.Size = new System.Drawing.Size(182, 29);
+            this.metroComboBoxServers.TabIndex = 44;
+            this.metroComboBoxServers.UseSelectable = true;
             // 
             // ReleaseHistory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.SlateGray;
-            this.Controls.Add(this.metroTextBox1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(134)))), ((int)(((byte)(163)))));
+            this.Controls.Add(this.groupBoxFilter);
+            this.Controls.Add(this.metroGridReleaseHistory);
             this.Name = "ReleaseHistory";
             this.Size = new System.Drawing.Size(680, 400);
+            this.Load += new System.EventHandler(this.metroGridReleaseHistory_Load);
+            this.Enter += new System.EventHandler(this.ReleaseHistory_Enter);
+            ((System.ComponentModel.ISupportInitialize)(this.metroGridReleaseHistory)).EndInit();
+            this.groupBoxFilter.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+
+        private MetroFramework.Controls.MetroGrid metroGridReleaseHistory;
+        private System.Windows.Forms.GroupBox groupBoxFilter;
+        private MetroFramework.Controls.MetroComboBox metroComboBoxServers;
+        private MetroFramework.Controls.MetroButton metroButtonFilter;
+        private MetroFramework.Controls.MetroComboBox metroComboBoxUsers;
     }
 }
--- Reserver/ReleaseHistory.cs
+++ Reserver/ReleaseHistory.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FirebirdSql.Data.FirebirdClient;
 
 namespace Reserver
 {
@@ -14,25 +15,110 @@ namespace Reserver
     {
         new public Reserver ParentForm { get; set; }
 
+        DataTable dataTableGrid;
+
         public ReleaseHistory()
         {
             InitializeComponent();
         }
-        public string ID
+
+        private void ReleaseHistory_Enter(object sender, EventArgs e)
         {
-            get { return metroTextBox1.Text; }
+            using (FbConnection connection = new FbConnection(ParentForm.ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (FbDataAdapter dataAdapterServerList = new FbDataAdapter("SELECT idserver, descrizione FROM servers", connection))
+                    {
+                        DataTable dataTableServerList = new DataTable();
+                        dataAdapterServerList.Fill(dataTableServerList);
+
+                        DataRow rowServerList = dataTableServerList.NewRow();
+                        rowServerList[0] = 0;
+                        dataTableServerList.Rows.InsertAt(rowServerList, 0);
+
+                        metroComboBoxServers.DataSource = dataTableServerList;
+                        metroComboBoxServers.DisplayMember = "descrizione";
+                        metroComboBoxServers.ValueMember = "idserver";
+                    }
+
+                    using (FbDataAdapter dataAdapterServerList = new FbDataAdapter("SELECT idutente, denominazione FROM utenti", connection))
+                    {
+                        DataTable dataTableServerList = new DataTable();
+                        dataAdapterServerList.Fill(dataTableServerList);
+
+                        DataRow rowServerList = dataTableServerList.NewRow();
+                        rowServerList[0] = 0;
+                        //row[1] = "Please select";
+                        dataTableServerList.Rows.InsertAt(rowServerList, 0);
+
+                        metroComboBoxUsers.DataSource = dataTableServerList;
+                        metroComboBoxUsers.DisplayMember = "denominazione";
+                        metroComboBoxUsers.ValueMember = "idutente";
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Richiesta al database fallita", "Errore DB", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
         }
 
-        private void metroTextBox1_TextChanged(object sender, EventArgs e)
+        private void metroGridReleaseHistory_Load(object sender, EventArgs e)
         {
-            var textBoxContent = this.metroTextBox1.Text;
-            //var parent = this.Parent as Reserver;
-            //ParentForm.ID = ID;
+            using (FbConnection connection = new FbConnection(ParentForm.ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (FbDataAdapter dataAdapterGrid = new FbDataAdapter(@"
+                        select s.DESCRIZIONE as Ambiente, sr.DESCRIZIONE, u.DENOMINAZIONE, sr.DATAINIZIO, sr.DATAFINE
+                        from STORICORILASCI sr 
+                        join SERVERS s on s.IDSERVER = sr.IDSERVER
+                        join utenti u on u.IDUTENTE = sr.IDUTENTE
+                        order by sr.DATAFINE nulls first, sr.DATAINIZIO desc", connection))
+                    {
+                        dataTableGrid = new DataTable();
+                        dataAdapterGrid.Fill(dataTableGrid);
+                        metroGridReleaseHistory.DataSource = dataTableGrid;
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Richiesta al database fallita", "Errore DB", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void metroButtonFilter_Click(object sender, EventArgs e)
         {
-            //metroTextBox1.Text = metroDateTime1.Text;
+            string filtro = "";
+
+            if (metroComboBoxServers.Text.Equals(""))
+            {
+                if (!metroComboBoxUsers.Text.Equals(""))
+                {
+                    filtro = "DENOMINAZIONE = '" + metroComboBoxUsers.Text + "'";
+                }
+            }
+            else
+            {
+                filtro = "ambiente = '" + metroComboBoxServers.Text + "'";
+                if (!metroComboBoxUsers.Text.Equals(""))
+                {
+                    filtro = filtro + " and DENOMINAZIONE = '" + metroComboBoxUsers.Text + "'";
+                }
+            }
+            dataTableGrid.DefaultView.RowFilter = filtro;
         }
     }
 }
