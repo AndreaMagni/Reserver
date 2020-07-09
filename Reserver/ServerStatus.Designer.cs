b832126430df4876a6df932782430e4c281a01bf
Andrea Magni <andrea.magni11@studenti.unimi.it>
2020-07-09 23:07:41 +0200

    Server Status

--- Reserver/ServerStatus.Designer.cs
+++ Reserver/ServerStatus.Designer.cs
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonUpdateStatus = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // buttonUpdateStatus
+            // 
+            this.buttonUpdateStatus.Location = new System.Drawing.Point(609, 18);
+            this.buttonUpdateStatus.Name = "buttonUpdateStatus";
+            this.buttonUpdateStatus.Size = new System.Drawing.Size(54, 23);
+            this.buttonUpdateStatus.TabIndex = 0;
+            this.buttonUpdateStatus.Text = "Update";
+            this.buttonUpdateStatus.UseVisualStyleBackColor = true;
+            this.buttonUpdateStatus.Click += new System.EventHandler(this.ButtonUpdateStatus_Click);
             // 
             // ServerStatus
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SlateGray;
+            this.Controls.Add(this.buttonUpdateStatus);
             this.Name = "ServerStatus";
             this.Size = new System.Drawing.Size(680, 400);
-            this.Load += new System.EventHandler(this.ServerStatus_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonUpdateStatus;
     }
 }
--- Reserver/ServerStatus.cs
+++ Reserver/ServerStatus.cs
@@ -11,46 +11,9 @@ namespace Reserver
     {
         new public Reserver ParentForm { get; set; }
 
-        //private bool firstTime = false;
-        //private Timer serverStatusTimer;
-
         public ServerStatus()
         {
             InitializeComponent();
-            //firstTime = true;
-            //this.Visible = false;
-        }
-
-        //public void InitTimer()
-        //{
-        //    serverStatusTimer = new Timer();
-        //    serverStatusTimer.Tick += new EventHandler(ServerStatusTimer_Tick);
-        //    serverStatusTimer.Interval = 10000;
-        //    serverStatusTimer.Start();
-        //}
-
-        //private void ServerStatusTimer_Tick(object sender, EventArgs e)
-        //{
-        //    UpdateStatus();
-        //}
-
-        //private void VisibleChange(object sender, EventArgs e)
-        //{
-        //    bool visible = this.Visible;
-
-        //    if (firstTime && visible)
-        //    {
-        //        FirstTimeLoad();
-        //        firstTime = false;
-        //    }
-        //}
-
-        private void ServerStatus_Load(object sender, EventArgs e)
-        {
-            //if(ParentForm.IsLogged)
-            //{
-            //    FirstTimeLoad();
-            //}
         }
 
         public void FirstTimeLoad()
@@ -89,14 +52,27 @@ namespace Reserver
 
                         Button button = new Button();
                         button.Left = left + 100;
+                        button.Size = new Size(200, 25);
                         button.Top = top;
                         button.Name = row["CODICE"].ToString();
-                        button.Text = "Rilascia";
+                        // Inizio verifica utenti con rilasci attivi che hanno chiuso l'applicazione
+                        string queryCheckRilasciAttivi = string.Format(@"SELECT * FROM servers s JOIN storicorilasci sr ON s.IDSERVER = sr.IDSERVER WHERE s.codice = '{0}' AND sr.idutente = {1} AND sr.attivo = 1", row["CODICE"].ToString(), ParentForm.CurrentUserID);
+                        FbCommand getCheckRilasciAttivi = new FbCommand(queryCheckRilasciAttivi, connection);
+                        FbDataReader readerGetCheckRilasciAttivi = getCheckRilasciAttivi.ExecuteReader();
+                        if (readerGetCheckRilasciAttivi.Read())
+                        {
+                            button.Text = "Rilascio concluso";
+                        }
+                        else
+                        {
+                            button.Text = "Rilascia";
+                        }
+                        // Fine verifica
                         button.Click += new EventHandler(this.ReleaseButton_Click);
                         this.Controls.Add(button);
 
                         Label panelLabel = new Label();
-                        panelLabel.Left = left + 200;
+                        panelLabel.Left = left + 350;
                         panelLabel.Top = top;
                         panelLabel.Size = new Size(25, 25);
                         panelLabel.Name = "status" + row["CODICE"].ToString();
@@ -115,6 +91,151 @@ namespace Reserver
                     connection.Close();
                 }
             }
+        }
+
+        private void ReleaseButton_Click(object sender, EventArgs e)
+        {
+            string buttonName = ((Control)sender).Name;
+            string buttonText = ((Control)sender).Text;
+
+            using (FbConnection connection = new FbConnection(ParentForm.ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    if (buttonText == "Rilascia")
+                    {
+                        IniziaRilascio(connection, buttonName);
+                    }
+                    else
+                    {
+                        ConcludiRilascio(connection, buttonName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Richiesta al database fallita", "Errore DB", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void IniziaRilascio(FbConnection connection, string buttonName)
+        {
+            string queryGetServerID = string.Format(@"SELECT s.idserver, ss.stato FROM servers s JOIN statiservers ss ON ss.idserver = s.idserver WHERE s.codice = '{0}'", buttonName);
+            FbCommand getServerID = new FbCommand(queryGetServerID, connection);
+            FbDataReader readerGetServerID = getServerID.ExecuteReader();
+
+            if (readerGetServerID.Read())
+            {
+                int serverID = readerGetServerID.GetInt32(0);
+                string serverStatus = readerGetServerID.GetString(1);
+
+                if (serverStatus == "LIBERO")
+                {
+                    string queryUpdateServerStatus = string.Format(@"UPDATE STATISERVERS SET STATO = 'OCCUPATO' WHERE IDSERVER = {0}", serverID);
+                    FbCommand updateServerStatus = new FbCommand(queryUpdateServerStatus, connection);
+                    updateServerStatus.ExecuteNonQuery();
+
+                    Button currentButton = this.Controls.Find(buttonName, true).FirstOrDefault() as Button;
+                    currentButton.Text = "Rilascio concluso";
+
+                    Label currentLabel = this.Controls.Find("status" + buttonName, true).FirstOrDefault() as Label;
+                    currentLabel.BackColor = Color.Red;
+
+                    string queryStatusInfo = string.Format(@"INSERT INTO STORICORILASCI (IDSERVER, IDUTENTE, attivo, DESCRIZIONE, DATAINIZIO) VALUES ({0}, {1}, {2}, '', '{3}')", serverID, ParentForm.CurrentUserID, 1, DateTime.Now);
+                    FbCommand getStatusInfo = new FbCommand(queryStatusInfo, connection);
+                    getStatusInfo.ExecuteNonQuery();
+                }
+                else
+                {
+                    MessageBox.Show("Rilascio in corso, aspettare o effettuare una prenotazione", "Rilascio", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Errore query lettura", "Stato connessione", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            }
+        }
+
+        private void ConcludiRilascio(FbConnection connection, string buttonName)
+        {
+            string queryGetServerID = string.Format(@"SELECT s.idserver, ss.stato FROM servers s JOIN statiservers ss ON ss.idserver = s.idserver WHERE s.codice = '{0}'", buttonName);
+            FbCommand getServerID = new FbCommand(queryGetServerID, connection);
+            FbDataReader readerGetServerID = getServerID.ExecuteReader();
+
+            if (readerGetServerID.Read())
+            {
+                int serverID = readerGetServerID.GetInt32(0);
+
+                string queryUpdateServerStatus = string.Format(@"UPDATE STATISERVERS SET STATO = 'LIBERO' WHERE IDSERVER = {0}", serverID);
+                FbCommand updateServerStatus = new FbCommand(queryUpdateServerStatus, connection);
+                updateServerStatus.ExecuteNonQuery();
+
+                Button currentButton = this.Controls.Find(buttonName, true).FirstOrDefault() as Button;
+                currentButton.Text = "Rilascia";
+
+                Label currentLabel = this.Controls.Find("status" + buttonName, true).FirstOrDefault() as Label;
+                currentLabel.BackColor = Color.Green;
+
+                string queryStatusInfo = string.Format(@"UPDATE storicorilasci SET attivo = 0, datafine = '{0}' WHERE idserver = {1} AND idutente = {2} AND attivo = 1", DateTime.Now, serverID, ParentForm.CurrentUserID);
+                FbCommand getStatusInfo = new FbCommand(queryStatusInfo, connection);
+                getStatusInfo.ExecuteNonQuery();
+            }
+            else
+            {
+                MessageBox.Show("Errore query lettura", "Stato connessione", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            }
+        }
+
+        private void ButtonUpdateStatus_Click(object sender, EventArgs e)
+        {
+            using (FbConnection connection = new FbConnection(ParentForm.ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string queryStatusInfo = string.Format(@"
+                        SELECT r.STATO, s.CODICE
+                        FROM STATISERVERS r
+                        JOIN SERVERS s ON s.IDSERVER = r.IDSERVER");
+                    FbCommand getStatusInfo = new FbCommand(queryStatusInfo, connection);
+                    FbDataReader readerGetServerID = getStatusInfo.ExecuteReader();
+
+                    while (readerGetServerID.Read())
+                    {
+                        Label currentLabel = this.Controls.Find("status" + readerGetServerID.GetString(1), true).FirstOrDefault() as Label;
+                        currentLabel.BackColor = (readerGetServerID.GetString(0) == "OCCUPATO") ? Color.Red : Color.Green;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Richiesta al database fallita", "Errore DB", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        /*
+        private Timer serverStatusTimer;
+
+        public void InitTimer()
+        {
+            serverStatusTimer = new Timer();
+            serverStatusTimer.Tick += new EventHandler(ServerStatusTimer_Tick);
+            serverStatusTimer.Interval = 10000;
+            serverStatusTimer.Start();
+        }
+
+        private void ServerStatusTimer_Tick(object sender, EventArgs e)
+        {
+            UpdateStatus();
         }
 
         private void UpdateStatus()
@@ -151,129 +272,7 @@ namespace Reserver
                 }
             }
         }
-
-        //private void VisibleChange(object sender, EventArgs e)
-        //{
-        //    bool visible = this.Visible;
-
-        //    if (firstTime && visible)
-        //    {
-        //        FirstTimeLoad();
-        //        firstTime = false;
-        //    }
-
-        //    if (visible)
-        //    {
-        //        InitTimer();
-        //    }
-        //}
-
-        private void ReleaseButton_Click(object sender, EventArgs e)
-        {
-            string labelName = ((Control)sender).Name;
-            int serverID = -1;
-
-            using (FbConnection connection = new FbConnection(ParentForm.ConnectionString))
-            {
-                try
-                {
-                    int serverId;
-                    string serverStatus;
-
-                    connection.Open();
-
-                    serverID = 1;
-
-                    /*
-                    string queryStatusInfo = string.Format(@"INSERT INTO STORICORILASCI (IDSERVER, IDUTENTE, STATO, DATAINIZIO) VALUES ({0}, {1}, {2}, '{3}')", serverID, ParentForm.CurrentUserID, 1, DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
-                    FbCommand getStatusInfo = new FbCommand(queryStatusInfo, connection);
-                    getStatusInfo.ExecuteNonQuery();
-                    */
-                    string queryGetServerID = string.Format(@"SELECT s.idserver, ss.stato FROM servers s JOIN statiservers ss ON ss.idserver = s.idserver WHERE s.codice = '{0}'", labelName);
-                    FbCommand getServerID = new FbCommand(queryGetServerID, connection);
-                    FbDataReader readerGetServerID = getServerID.ExecuteReader();
-                    serverID = readerGetServerID.GetInt32(0);
-                    serverId = readerGetServerID.GetInt32(0);
-                    serverStatus = readerGetServerID.GetString(1);
-
-                    if (serverStatus == "OCCUPATO")
-                    {
-                        MessageBox.Show("Rilascio in corso, aspettare o effettuare una prenotazione", "Rilascio", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
-                    }
-                    else
-                    {
-                        string queryStatusInfo = string.Format(@"
-                        INSERT INTO STORICORILASCI (IDSERVER, IDUTENTE, STATO)
-                        VALUES ({0}, {1}, {2})", serverID, ParentForm.CurrentUserID, 1);
-                        FbCommand getStatusInfo = new FbCommand(queryStatusInfo, connection);
-                        getStatusInfo.ExecuteNonQuery();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Richiesta al database fallita", "Errore DB", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
-                }
-                finally
-                {
-                    connection.Close();
-                }
-            }
-        }
-
-
-        //private void metroButton1_Click(object sender, EventArgs e)
-        //{
-        //    if (ParentForm == null)
-        //    {
-        //        MessageBox.Show("Errore parent form", "Errore parent form", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
-        //    }
-
-        //    using (FbConnection connection = new FbConnection(ParentForm.ConnectionString))
-        //    {
-        //        try
-        //        {
-        //            int serverId;
-        //            string serverStatus;
-        //            connection.Open();
-        //            string queryServerInfo = string.Format(@"SELECT s.idserver, ss.stato
-        //                                                     FROM servers s
-        //                                                     JOIN statiservers ss ON ss.idserver = s.idserver
-        //                                                     WHERE s.descrizione = '{0}'", server["metroButton1"]);
-        //            FbCommand getServerInfo = new FbCommand(queryServerInfo, connection);
-        //            FbDataReader readerServerInfo = getServerInfo.ExecuteReader();
-        //            serverId = readerServerInfo.GetInt32(0);
-        //            /* Non dovrebbe essere necessario fare tutto questo, andrà sempre a buon fine
-        //                if (readerServerInfo.Read())
-        //                {
-        //                    serverID = readerServerInfo.GetInt32(0); 
-        //                    MessageBox.Show(serverID.ToString(), "", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
-        //                }
-        //                else
-        //                {
-        //                    MessageBox.Show("Utente non censito a sistema", "Stato connessione", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
-        //                }
-        //            */
-        //            serverStatus = readerServerInfo.GetString(1);
-
-        //            if (serverStatus == "OCCUPATO")
-        //            {
-        //                MessageBox.Show("Rilascio in corso, aspettare o effettuare una prenotazione", "Rilascio", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
-        //            }
-        //            else
-        //            {
-
-        //            }
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            MessageBox.Show("Richiesta al database fallita", "Errore DB", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
-        //        }
-        //        finally
-        //        {
-        //            connection.Close();
-        //        }
-        //    }
-        //}
+        */
 
     }
 }
