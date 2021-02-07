c71d445bb3a172ce50b1eb58483949eac2e5a8d7
Andrea Magni <andrea.magni@hotmail.it>
2021-02-07 20:19:02 +0100

    #26
    Startup + Fix icona SystemTray

--- Reserver/Reserver.cs
+++ Reserver/Reserver.cs
@@ -6,12 +6,15 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using IWshRuntimeLibrary;
+using Shell32;
 
 namespace Reserver
 {
     public partial class Reserver : Form
     {
         public string connectionStringExternalDB = ConfigurationManager.AppSettings["ConnectionString"];
+        public bool startup = Convert.ToBoolean(ConfigurationManager.AppSettings["Startup"]);
         public bool logged = false;
         public int idutente = -1;
         public object emptySender = new object();
@@ -39,6 +42,22 @@ namespace Reserver
             btnSideMenuAcceptanceTests.Font = new Font(privateFontCollection.Families[0], 15, FontStyle.Bold);
             labelCurrentUserV2.Font = new Font(privateFontCollection.Families[0], 11, FontStyle.Regular);
             reserverNotifyIcon.Visible = true;
+            string fileName = Environment.GetFolderPath(Environment.SpecialFolder.Startup) + "\\" + Application.ProductName + ".lnk";
+            if (startup)
+            {
+                if (string.IsNullOrEmpty(GetShortcutTargetFile(fileName)))
+                {
+                    CreateStartupFolderShortcut();
+                }
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(GetShortcutTargetFile(fileName)))
+                {
+                    DeleteStartupFolderShortcuts(Application.ProductName + ".exe");
+                }
+
+            }
         }
 
         #region Getter and Setter
@@ -103,6 +122,7 @@ namespace Reserver
         private void BtnClose_Click(object sender, EventArgs e)
         {
             reserverNotifyIcon.Visible = false;
+            reserverNotifyIcon.Dispose();
             Application.Exit();
         }
 
@@ -188,6 +208,49 @@ namespace Reserver
 
         #endregion Side Menu Behavior
 
+        #region Startup
+        public void CreateStartupFolderShortcut()
+        {
+            WshShellClass wshShell = new WshShellClass();
+            IWshShortcut shortcut;
+            string startUpFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            shortcut = (IWshShortcut)wshShell.CreateShortcut(startUpFolderPath + "\\" + Application.ProductName + ".lnk");
+            shortcut.TargetPath = Application.ExecutablePath;
+            shortcut.WorkingDirectory = Application.StartupPath;
+            shortcut.Description = "Launch Reserver Application";
+            shortcut.Save();
+        }
+
+        public string GetShortcutTargetFile(string shortcutFilename)
+        {
+            string pathOnly = Path.GetDirectoryName(shortcutFilename);
+            string filenameOnly = Path.GetFileName(shortcutFilename);
+            Shell shell = new ShellClass();
+            Shell32.Folder folder = shell.NameSpace(pathOnly);
+            FolderItem folderItem = folder.ParseName(filenameOnly);
+            if (folderItem != null)
+            {
+                ShellLinkObject link = (ShellLinkObject)folderItem.GetLink;
+                return link.Path;
+            }
+            return String.Empty; 
+        }
+
+        public void DeleteStartupFolderShortcuts(string targetExeName)
+        {
+            string startUpFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            DirectoryInfo di = new DirectoryInfo(startUpFolderPath);
+            FileInfo[] files = di.GetFiles("*.lnk");
+            foreach (FileInfo fi in files)
+            {
+                string shortcutTargetFile = GetShortcutTargetFile(fi.FullName);
+                if (shortcutTargetFile.EndsWith(targetExeName, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    System.IO.File.Delete(fi.FullName);
+                }
+            }
+        }
+        #endregion Startup
     }
 }
 
